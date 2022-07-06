81c6fc357a58ca49c815bbe94264c71b13e08796
ChickenBenny <zxc123benny14159@gmail.com>
2022-07-06 16:53:25 +0800

    update the code and complete the account function

--- Social_Network/__init__.py
+++ Social_Network/__init__.py
@@ -1,27 +1,21 @@
-from flask import Flask
+import bcrypt
+from os import path
+from flask import Flask  
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from os import path
 
-DB_NAME = "database.db"
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'Bennys Project'
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+app.config['SECRET_KEY'] = 'Benny'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+
+
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
+from . import routes
 
-
-
-from .views import views
-from .auth import auth
-
-app.register_blueprint(views, url_prefix = '/')
-app.register_blueprint(auth, url_prefix = '/')
-
-from .models import User, Post
-    
